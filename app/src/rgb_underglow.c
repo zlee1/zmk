ac03c88c6f66450beec62b1ade7e153dc3f01f4b
zlee1 <zack.lee@valpo.edu>
2026-01-02 17:35:24 -0500

    identifying rgb 0

--- app/src/rgb_underglow.c
+++ app/src/rgb_underglow.c
@@ -201,13 +201,18 @@ _Bool pressed[STRIP_NUM_PIXELS];
 // custom effect - reactive
 static void zmk_rgb_underglow_effect_reactive(void) {
     for (int i = 0; i < STRIP_NUM_PIXELS; i++) {
-        if (pressed[i] == 1) {
+        if(state.animation_step%20 == 0){
             struct zmk_led_hsb hsb = state.color;
-            hsb.b = 0;
-            // hsb.b = abs(state.animation_step - 1200) / 12;
-
-            pixels[i] = hsb_to_rgb(hsb_scale_zero_max(hsb));
+            hsb.h = rand()%HUE_MAX;
+            pixels[0] = hsb_to_rgb(hsb_scale_zero_max(hsb));
         }
+        // if (pressed[i] == 1) {
+        //     struct zmk_led_hsb hsb = state.color;
+        //     hsb.b = 0;
+        //     // hsb.b = abs(state.animation_step - 1200) / 12;
+
+        //     pixels[i] = hsb_to_rgb(hsb_scale_zero_max(hsb));
+        // }
     }
 
     state.animation_step += state.animation_speed * 10;
